59d905e8ca6ea4bc9a30a783ccc462af74b32147
Nikolay Popov <mail@lifebits.ru>
2018-02-25 16:54:09 +0700

    angular universal update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { TransferState, makeStateKey } from '@angular/platform-browser';
+
+const DOGS_KEY = makeStateKey('dogs');
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'app';
+  dogs?: any;
+
+  constructor(
+    private http: HttpClient,
+    private state: TransferState) {
+  }
+
+  ngOnInit() {
+    this.dogs = this.state.get(DOGS_KEY, null as any);
+    if (!this.dogs) {
+      this.http
+        .get('https://dog.ceo/api/breeds/list/all')
+        .subscribe(response => {
+          this.dogs = response;
+          this.state.set(DOGS_KEY,response as any);
+        })
+    }
+  }
+
 }
